5a3ad54e60a6dbe9ede0df8418171cde5d5cafb9
Deborah Newberry <dnewberry16@gmail.com>
2017-02-14 22:40:34 -0800

    added filter and clear filter to roster

--- Iota Pi App/RosterViewController.swift
+++ Iota Pi App/RosterViewController.swift
@@ -7,63 +7,96 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class RosterTableViewCell: UITableViewCell {
-    @IBOutlet weak var rosterLabel: UILabel!
     var brotherId: String!
-    
 }
 
-class RosterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-//    @IBOutlet weak var createUserButton: UIBarButtonItem!
-    @IBOutlet weak var rosterTable: UITableView!
-
-    let brothersArray = Array(RosterManager.sharedInstance.brothersMap.values)
+class RosterTableViewController: UITableViewController {
+    @IBOutlet weak var clearFilterButton: UIBarButtonItem!
+    var brothersArray: [User] = Array(RosterManager.sharedInstance.brothersMap.values)
     var chosenBrotherId: String!
+    var filter = ""
+    
+    @IBAction func searchByName(_ sender: AnyObject) {
+        let searchByNameAlert = SCLAlertView()
+        let searchField = searchByNameAlert.addTextField()
+        searchField.autocapitalizationType = .none
+        searchField.autocorrectionType = .no
+        searchField.placeholder = "Name"
+        
+        searchByNameAlert.showEdit("Search Roster", subTitle: "").setDismissBlock {
+            if let search = searchField.text {
+                self.filter = search
+                self.filterRoster()
+            }
+        }
+    }
+    
+    @IBAction func clearFilter(_ sender: AnyObject) {
+        self.filter = ""
+        self.filterRoster()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.rosterTable.tableFooterView = UIView()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.tableView.tableFooterView = UIView()
+        self.clearFilterButton.isEnabled = false
+        self.clearFilterButton.tintColor = UIColor.clear
     }
     
-
-    func numberOfSections(in tableView: UITableView) -> Int {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return RosterManager.sharedInstance.brothersMap.count
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.brothersArray.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "rosterCell", for: indexPath) as! RosterTableViewCell
         
         let currentBrother = brothersArray[indexPath.row]
         
         cell.brotherId = currentBrother.userId
-        cell.rosterLabel.text = currentBrother.firstname + " " + currentBrother.lastname
+        print("🛎🛎🛎🛎🛎🛎🛎🛎🛎🛎 + \(currentBrother.userId)")
+        cell.textLabel!.text = currentBrother.firstname + " " + currentBrother.lastname
         
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //let currentCell = tableView.dequeueReusableCell(withIdentifier: "rosterCell", for: indexPath) as! RosterTableViewCell
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) as? RosterTableViewCell {
             chosenBrotherId = cell.brotherId
             performSegue(withIdentifier: "rosterDetailSegue", sender: self)
         }
     }
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "rosterDetailSegue" {
             let destination = segue.destination as! RosterDetailViewController
             destination.currentBrotherId = chosenBrotherId
         }
+    }
+    
+    func filterRoster() {
+        if !filter.isEmpty {
+            self.brothersArray = self.brothersArray.filter({
+                $0.firstname.lowercased().contains(filter.lowercased()) || $0.lastname.lowercased().contains(filter.lowercased())
+            })
+            self.clearFilterButton.isEnabled = true
+            self.clearFilterButton.tintColor = nil
+        } else {
+            self.brothersArray = Array(RosterManager.sharedInstance.brothersMap.values)
+            self.clearFilterButton.isEnabled = false
+            self.clearFilterButton.tintColor = UIColor.clear
+        }
+        self.tableView.reloadData()
     }
 }
