771e8b696d6f33014eba602b931d959708ba0b7e
Deborah Newberry <dnewberry16@gmail.com>
2017-03-01 14:13:11 -0800

    setting up beta

--- Iota Pi App/AppDelegate.swift
+++ Iota Pi App/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
-        
+        Fabric.with([Crashlytics.self])
+
         // UINavigationBar appearance
         UINavigationBar.appearance().tintColor = UIColor.white
         UINavigationBar.appearance().barTintColor = Style.mainColor
